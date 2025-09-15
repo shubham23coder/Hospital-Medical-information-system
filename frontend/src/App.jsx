0b891c719b1fce6872ca0d5fe0612b2c6dee3c5c
shubham23coder <shubhamjigupta23@gmail.com>
2025-09-15 19:04:49 +0530

    upload frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/doctors" element={<Doctors />}></Route>
+        <Route path="/doctors/:speciality" element={<Doctors />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/my-profile" element={<MyProfile />}></Route>
+        <Route path="/my-appointments" element={<MyAppointments />}></Route>
+        <Route path="/appointment/:docId" element={<Appointment />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+  // const currencySymbol = "$";
+  // const backendUrl = "http://localhost:4000";
+  // const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://doctorallotment.onrender.com";
+  const currencySymbol = import.meta.env.CURRENCY;
+
+  const [userData, setUserData] = useState(false);
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: { token },
+      });
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : false
+  );
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const value = {
+    doctors,
+    getDoctorsData,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
+
+  return (
+    // eslint-disable-next-line react/prop-types
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
